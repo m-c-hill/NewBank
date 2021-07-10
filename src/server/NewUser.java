405d24df8a3fa0b6d9011ede77a7638944ecee2e
Simon Royle <69103179+kezuk23@users.noreply.github.com>
2021-07-10 23:13:39 +1200

    Update NewUser.java

--- src/server/NewUser.java
+++ src/server/NewUser.java
@@ -3,67 +3,87 @@ package server;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
 public class NewUser {
     
     private BufferedReader in;
+	private PrintWriter out;
     
     public void createNewUser(String s){
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    
+        in = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(out, true);
+
         try {
         // ask for first name
-        System.out.println("Enter First Name");
+        out.println("Enter First Name");
         String firstName = in.readLine();
         // ask for last name
-        System.out.println("Enter Surname");
+        out.println("Enter Surname");
         String surname = in.readLine();
 
         //social security ID
-        System.out.println("Enter SSN");
+        out.println("Enter SSN");
         String ssn = in.readLine();
 
         //dob
-        System.out.println("Enter Date of Birth in DDMMYYYY format");
+        out.println("Enter Date of Birth in DDMMYYYY format");
         String dob = in.readLine();
 
         //validate - later
         
         //place of birth
-        System.out.println("Enter Place of Birth");
+        out.println("Enter Place of Birth");
         String placeOfBirth = in.readLine();
 
         //mobile phone number
-        System.out.println("Enter Mobile Phone Number");
+        out.println("Enter Mobile Phone Number");
         String mobileNummber = in.readLine();
 
         //email
-        System.out.println("Enter email address");
+        out.println("Enter email address");
         String email = in.readLine();
 
         //address
-        System.out.println("Enter First Line of Address");
+        out.println("Enter First Line of Address");
         String addressFirstLine = in.readLine();
 
-        System.out.println("Enter Second Line of Address");
+        out.println("Enter Second Line of Address");
         String addressSecondLine = in.readLine();
 
-        System.out.println("Enter Town or City");
+        out.println("Enter Town or City");
         String townOrCity = in.readLine();
 
-        System.out.println("Enter Postcode");
+        out.println("Enter Postcode");
         String postcode = in.readLine();
 
         //password
-        System.out.println("Enter a valid password");
-        
+        passwordGenerator();
         //password method with instructions
-        String password = in.readLine();
+        
 
      } catch (IOException e) {
         
         }
 
-    }  
+    }
+
+    //add all data to server
+    //use a hashtable? 
+
+    //add account to server 
+    Account newAccount = new Account(accountName, openingBalance);
+
+    
+
+    public String passwordGenerator() throws IOException {
+    //password instructions
+    out.println("Please create a new password");
+    out.println("Passwords must be...");
+    
+    String password = in.readLine();
+    return password;
+        
+    }
 }
