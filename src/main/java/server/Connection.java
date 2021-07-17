7b8dd635a9f72c9dd042c8ec0aaac99e26a5241d
m-c-hill <matthewhill2595@gmail.com>
2021-07-17 12:05:16 +0100

    Database connection finally working!

--- src/main/java/server/Connection.java
+++ src/main/java/server/Connection.java
@@ -14,24 +14,23 @@ import org.json.simple.parser.ParseException;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.DriverManager;
 import java.util.List;
-
-// class AWSCredentialManager
 
 public class Connection {
 
 	/**
-	 * Simple method to retrieve aws credentials from .secrets/credentials.json
+	 * Simple method to retrieve aws credentials from .secrets/aws_credentials.json
 	 * @return an array containing the access key and secret key
 	 */
-	private static String[] getCredentials(){
+	private static String[] getAWSCredentials(){
 		String key = null;
 		String pw = null;
 
 		JSONParser parser = new JSONParser();
 		try{
 			// Read in credentials json file, parse to create a json object
-			FileReader credFile = new FileReader(".secrets/credentials.json");
+			FileReader credFile = new FileReader(".secrets/aws_credentials.json");
 			Object obj = parser.parse(credFile);
 			JSONObject credJson = (JSONObject) obj;
 
@@ -39,32 +38,54 @@ public class Connection {
 			key = (String) credJson.get("access_key");
 			pw = (String) credJson.get("secret_key");
 		}
-		catch(FileNotFoundException e){
+		catch(Exception e){
 			e.printStackTrace();
 		}
-		catch(IOException e){
-			e.printStackTrace();
-		}
-		catch(ParseException e){
-			e.printStackTrace();
+		// TODO: change above catch block to logging output; unique way to handle each exception
+		return new String[] {key, pw};
+	}
+
+	/*
+	private static String[] getDBCredentials(){
+		String instance = null;
+		String endpoint = null;
+		String port = null;
+		String user = null;
+		String password = null;
+		String region = null;
+
+		JSONParser parser = new JSONParser();
+		try{
+			// Read in credentials json file, parse to create a json object
+			FileReader credFile = new FileReader(".secrets/db_credentials.json");
+			Object obj = parser.parse(credFile);
+			JSONObject credJson = (JSONObject) obj;
+
+			// Index credJson to retrieve key and secret
+			instance = (String) credJson.get("instance");
+			endpoint = (String) credJson.get("endpoint");
+			port = (String) credJson.get("port");
+			user = (String) credJson.get("user");
+			password = (String) credJson.get("password");
+			region = (String) credJson.get("region");
 		}
 		catch(Exception e){
 			e.printStackTrace();
 		}
-		// TODO: change above catch messages to logging output; unique way to handle each exception
-		return new String[] {key, pw};
+		return new String[] {instance, endpoint, port, user, password, region};
 	}
+	*/
 
 	/**
 	 * Method to create the connection object for the aws database instance
 	 * @return AmazonRDS database connection object
 	 */
-	public static AmazonRDS getConnection(){
+	private static AmazonRDS getRDSConnection(){
 
 		AmazonRDS awsRDS = null;
 
 		// Get the credentials used to connect to the database
-		String[] credArray = getCredentials();
+		String[] credArray = getAWSCredentials();
 		String accessKey = credArray[0];
 		String secretKey = credArray[1];
 
@@ -87,10 +108,37 @@ public class Connection {
 		return awsRDS;
 	}
 
+	public static java.sql.Connection getDBConnection() {
+		AmazonRDS awsRDS = getRDSConnection();
+		DescribeDBInstancesResult dbInstResult = awsRDS.describeDBInstances();
+		DBInstance dbInstance = dbInstResult.getDBInstances().get(0);
+
+		String name = dbInstance.getDBInstanceIdentifier();
+		String engine = dbInstance.getEngine();
+		String username = dbInstance.getMasterUsername();
+		String endpoint = dbInstance.getEndpoint().getAddress();
+		String port = dbInstance.getEndpoint().getPort().toString();
+		String password = "password";
+
+		java.sql.Connection con = null;
+
+		String url = "jdbc:" + engine + "://" + endpoint + ":" + port + "/" + name + "?user" + username + "&password=" + password;
+		System.out.println(url);
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection(url, username, "password");
+			System.out.println("Success!");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return con;
+	}
+
 	public static void main(String[] args) {
 
 		// Example of retrieving data - retrieves all database instances associated with the connection
-		AmazonRDS awsRDS = getConnection();
+		AmazonRDS awsRDS = getRDSConnection();
 		DescribeDBInstancesResult dbInstResult = awsRDS.describeDBInstances();
 
 		// Convert this into a list object and then loop through the list to print the instances returned by our query.
@@ -102,5 +150,7 @@ public class Connection {
 			System.out.println("DB Instance:: " + dbInst);
 			System.out.println("-------------------------\n");
 		}
+
+		java.sql.Connection con = getDBConnection();
 	}
 }
