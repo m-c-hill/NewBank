55e02c2744dd3bbc2db6a5181769988adab128c1
RaidenAllen <raidenallen@outlook.com>
2021-07-21 20:15:14 +0300

    Added showLoansList method

--- src/main/java/server/Admin.java
+++ src/main/java/server/Admin.java
@@ -13,12 +13,15 @@ public class Admin extends User {
         this.adminRoles = adminRoles;
     }
 
+    // Showing the loans list
     public void showLoansList(ArrayList<BankLoan> loansList, PrintWriter out) {
         if (this.adminRoles.isAllowedToViewLoanList()) {
-            out.println("Customer Name:\2tLoan Reason:\2tIs Accepted?\2tIs Paid Back?");
+            out.println("Customer Name\2tLoan Reason\2tAmount\2tAmount to Pay Back\2tAccepted?\2tPaid Back?");
             for (BankLoan bankLoan : loansList) {
-                out.print(bankLoan.getCustomer() + "\2t" 
+                out.print(bankLoan.getCustomer().getFirstName() + " " + bankLoan.getCustomer().getLastName() + "\2t" 
                 + bankLoan.getReason() + "\2t" 
+                + bankLoan.getAmount() + "\2t"
+                + bankLoan.getPayBackAmount() + "\2t"
                 + bankLoan.isAccepted()+ "\2t" 
                 + bankLoan.isPaidBack());
             }
@@ -26,5 +29,9 @@ public class Admin extends User {
             out.println("You're not authorized to access the loans list.");
         }
     }
+
+    // Approving a loan request
+
+    // Declining a loan request
 
 }
