a65fd693a48c4c1dc2f368b30a2f9afd0c4bc33a
m-c-hill <matthewhill2595@gmail.com>
2021-08-01 12:21:48 +0100

    Merged the Account and Balance classes

--- src/main/java/server/account/Account.java
+++ src/main/java/server/account/Account.java
@@ -1,56 +1,78 @@
 package server.account;
 
 import server.bank.Bank;
+import server.database.DbUtils;
 import server.database.GetObject;
-import server.user.Customer;
 
-import java.util.ArrayList;
-
+/**
+ * Class to represent a customer's bank account. A user may have multiple accounts, each with an assigned currency.
+ */
 public class Account {
-
-	private final int customerId;
 	private final String accountNumber;
 	private final String accountName;
 	private final Bank bank;
 	private final String statementSchedule = "monthly";
 	private double balance;
-	private Currency currency;
+	private final Currency currency;
 
-	public Account(Customer customer, String accountName, double openingBalance, Currency currency) {
-		this.customerId = customer.getCustomerId();
+	public Account(String accountName, double openingBalance, Currency currency) {
 		this.accountNumber = "00000001";  // generateNewAccountNumber(); // TODO: find a way to generate unique account IDs to replace user input
 		this.accountName = accountName;
-		this.bank = GetObject.getBank(1); // Returns default bank
+		this.bank = GetObject.getBank(1); // Returns default bank - for now all new accounts are "NewBank" accounts
 		this.balance = openingBalance;
 		this.currency = currency;
 	}
 
-	public String toString() {
-		Balance primaryBalance = getPrimaryBalance();
-		return (this.accountNumber + ": " + primaryBalance.getBalance() + primaryBalance.getCurrency().toString());
+	public double getBalance(){
+		return this.balance;
+	}
+
+	public Currency getCurrency(){
+		return this.currency;
+	}
+
+	public String getAccountNumber(){
+		return accountNumber;
+	}
+
+	public Bank getBank(){
+		return this.bank;
 	}
 
 	public String getAccountName() {
 		return accountName;
 	}
 
-	public double getBalance(){
-
+	/**
+	 * Method to return a basic account summary
+	 * @return String containing the account number, balance and currency type
+	 */
+	public String toString() {
+		return (this.accountNumber + ": " + getBalance() + " " + getCurrency().getName());
 	}
 
+	/**
+	 * Method to withdraw a set amount from the user's account
+	 * @param amount Amount to withdraw
+	 */
 	public void withdrawAmount(double amount){
-		double currentBalance = getPrimaryBalance().getBalance();
-		updatePrimaryBalance(currentBalance - amount);
+		updateBalance(this.balance - amount);
 	}
 
+	/**
+	 * Method to deposit a set amount into the user's account
+	 * @param amount Amount to withdraw
+	 */
 	public void makeDeposit(double amount) {
-		double currentBalance = getPrimaryBalance().getBalance();
-		updatePrimaryBalance(currentBalance + amount);
+		updateBalance(this.balance + amount);
 	}
 
+	/**
+	 * Method to pay a loan back
+	 * @param amount Loan amount due
+	 */
 	public void payBackLoan(double amount){
-		double currentBalance = getPrimaryBalance().getBalance();
-		updatePrimaryBalance(currentBalance - amount);
+		updateBalance(this.balance - amount);
 	}
 
 	public void executeTransaction(){
@@ -61,18 +83,17 @@ public class Account {
 		// TODO: create method and accompanying class to carry out transfers between accounts and log them in the database
 	}
 
-	private void updatePrimaryBalance(double newBalance){
-		// Method to update the primary balance for an account
-		Balance primaryBalance = getPrimaryBalance();
-		primaryBalance.updateBalance(newBalance);
+	public void updateBalance(double newBalance){
+		this.balance = newBalance; // Update the object balance
+		DbUtils.updateBalance(); // Update the balance in the database
+	}
+
+	private void generateNewAccountNumber(){
+		// TODO: ASAP
 	}
 
 	public void closeAccount(){
 		// TODO: create method to close account
-	}
-
-	public void addNewBalance(Currency currency, double openingBalance){
-		// TODO: create method to add a new balance to the account (ie. a new currency)
 	}
 
 	public void updateStatementSchedule(){
@@ -82,31 +103,4 @@ public class Account {
 	public void sendStatement(){
 		// TODO: method to send a summary of transactions for a given period
 	}
-
-	public String getAccountNumber(){
-		return accountNumber;
-	}
-
-	public double getBalance(){
-		return this.amount;
-	}
-
-	public String getCurrency(){
-		return this.currency.toString();
-	}
-
-	public void setAmount(double amount) {
-		this.amount = amount;
-	}
-
-	public void updateBalance(double newBalance){
-		this.amount = newBalance;
-		// TODO: update the corresponding balance in the database
-		// Method to update the balance amount in the database after a transaction has occurred
-	}
-
-	private void generateNewAccountNumber(){
-
-	}
-
 }
