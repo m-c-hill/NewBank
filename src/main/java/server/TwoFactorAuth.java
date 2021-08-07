0edbeead9e48aa1607a119314230a84da15ce0b2
Simon Royle <69103179+kezuk23@users.noreply.github.com>
2021-08-08 00:57:49 +1200

    Update TwoFactorAuth.java
    Okay I've initialised the BufferedReader and added the ISR - I'm not sure if the codeInput method is correct now that it's expected to take an argument. These were the suggested changes in VS code and while I know it may not be perfect, it eliminated the errors!
    
    As it's late here and I know we have a deadline to meet, feel free to make any further changes over the next 8 hours or so and I'll catch up with anything else that needs doing in the morning.

--- src/main/java/server/TwoFactorAuth.java
+++ src/main/java/server/TwoFactorAuth.java
@@ -1,8 +1,11 @@
 package server;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Random;
+import java.io.InputStreamReader;
+import java.net.Socket;
 
 public class TwoFactorAuth {
 
@@ -17,11 +20,10 @@ public class TwoFactorAuth {
         return expectedCode;
     }
 
-    public static void codeInput() {
+    public static void codeInput(Socket socket) throws IOException {
 
-        BufferedReader in;
-        PrintWriter out;
-
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
         // authorisation false until correct input
         boolean authentication = false;
 
@@ -37,13 +39,13 @@ public class TwoFactorAuth {
         }
     }
 
-    public static boolean twoFactorAuthentication() {
+    public static boolean twoFactorAuthentication() throws IOException {
 
         Sms.sendText(generateCode());
 
         System.out.println("Your one-time passcode has been sent to your phone");
 
-        codeInput();
+        codeInput(null);
 
         return authentication;
     }
